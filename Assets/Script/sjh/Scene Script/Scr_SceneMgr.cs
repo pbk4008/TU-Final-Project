0a52918a061ac4486cbd57847b7835da779ac940
LAPTOP-7ER0M5I0\TiPiRu <ths7789@naver.com>
2020-11-23 16:43:35 +0900

    던전입장UI 구현

---
+++ Assets/Script/sjh/Scene Script/Scr_SceneMgr.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scr_SceneMgr : MonoBehaviour
+{
+    private void Click_StageBtn()
+    {
+        SceneManager.LoadScene("scene2");
+    }
+}
